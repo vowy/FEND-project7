1ddde6e120692e13507541e44c54c2d79384ea7f
Eric Parsons <e.b.parsons@gmail.com>
2018-11-09 22:32:03 -0600

    Maps shows up, heading text added. Default coordinates changed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
 class App extends Component {
   static defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33
+      lat: 30.3929697,
+      lng: -91.238454
     },
-    zoom: 11
+    zoom: 12
   };
 
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: '85vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyBxxOcj6qFxS367L-6gXDTKTeoW-qQKNiY" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            zoom={10}
-          />
+
         </GoogleMapReact>
       </div>
     );
