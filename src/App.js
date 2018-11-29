bfff554a04a3f798cd1a0e4033c67e082ad98d43
Eric Parsons <e.b.parsons@gmail.com>
2018-11-29 11:08:42 -0600

    HUGE UPDATE, almost ready for review!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
+import { slide as Menu } from 'react-burger-menu';
 import {Marker, Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
-
-
+import './App.css';
+import Drawer from './Drawer.js';
 
 
 class App extends Component {
@@ -10,20 +11,32 @@ class App extends Component {
       lat: 30.3929697,
       lng: -91.238454
     },
-    zoom: 15,
+    zoom: 14,
+
   }
   constructor(props) {
      super(props);
      this.state={
-       markers: [],
-       markerProps: [],
+       mapProps: [],
        activeMarker: null,
        activeMarkerProps: null,
        showingInfoWindow: false,
-       currentLocation: [],
-       locations: []
+       filteredLocations: [],
+       query: '',
      }
    }
+
+
+  onQueryUpdate = (val) => {
+  this.setState({query:val});
+  this.setState({filteredLocations: (this.state.locations.filter(place => place.name.toLowerCase().includes(this.state.query.toLowerCase())))});
+}
+
+
+  clearFilter = () => {
+    this.setState({filteredLocations: this.state.locations})
+    this.setState({query: ''})
+  }
 
   postFormData = (url, data) => {
     return fetch(url, {
@@ -36,58 +49,38 @@ class App extends Component {
     })
   }
 
-
-componentDidMount() {
+apiSearchLocation = () =>
   this.postFormData('http://api.geonames.org/search', {username: 'vowy', north: 30.40815, west: -91.278549, south: 30.380068, east: -91.225334, type: "json", style: "SHORT"})
    .then(results => results.json())
    .then(data => {
-     return this.setState({locations: data.geonames})
+     return this.setState({locations: data.geonames, filteredLocations: data.geonames});
    })
- }
+
+
+componentDidMount() {
+  this.apiSearchLocation();
+}
 
 
 mapReady = (props, map) => {
    this.setState({map});
-   this.updateMarkers(this.state.locations)
+   this.setState({mapProps: props});
  }
 
-closeInfoWindow = () => {
-  this.state.activeMarker && this.state.activeMarker.setAnimation(null);
-  this.setState({showingInfoWindow: false, activeMarker: null, activeMarkerProps:null})
+closeInfoWindow = (props, marker) => {
+  this.setState({showingInfoWindow: false, activeMarker: null, activeMarkerProps: null})
 }
 
 onMarkerClick = (props, marker, e) => {
-  this.closeInfoWindow();
+  this.closeInfoWindow(props, marker);
+  marker.setAnimation(this.props.google.maps.Animation.BOUNCE)
   this.setState({showingInfoWindow: true, activeMarker: marker, activeMarkerProps: props});
 }
 
-updateMarkers = (locations) => {
 
-  this.state.markers.forEach(marker => marker.setMap(null));
-  let markerProps = [];
-  let marker = locations.map((location, index) => {
-    let mProps = {
-      key: index,
-      index,
-      name: location.name,
-      position: (location.lat, location.lng)
-    };
-    markerProps.push(mProps);
 
-    let animation = this.props.google.maps.Animation.DROP;
-    let marker = new this.props.google.maps.Marker({
-      position: (location.lat, location.lng),
-      map: this.state.map,
-      animation
-    });
-    marker.addListener('click', () => {
-      this.onMarkerClick(mProps, marker, null);
-    });
-    return marker;
-  })
-  this.setState({marker, markerProps});
-}
   render() {
+
 
     let amProps = this.state.activeMarkerProps
 
@@ -97,35 +90,47 @@ updateMarkers = (locations) => {
       lng: this.props.center.lng
     }
 
-    const style={ height: '85vh', width: '100%' }
+    const mapStyle={ height: '90%', width: 'auto', overflow: 'hidden' }
+
 
 
     return (
-      <div className="App">
-        <div id= "heading-text">
-          <h1>Neighborhood Map of Brusly, Louisiana</h1>
-        </div>
-        <Map
-          aria-label="map"
+      <div className='App' id='outer-container'>
+        <Menu bodyClassName={ "menuBody" } pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
+        <Drawer
+          clearFilter={this.clearFilter.bind(this)}
+          onQueryUpdate={this.onQueryUpdate.bind(this)}
+          filteredLocations={this.state.filteredLocations}
+          locations={this.state.locations}
+          searchedQuery={this.state.query}
+          />
+
+      </Menu>
+
+        <Map id='page-wrap'
+          aria-label='map'
           initialCenter={center}
           zoom={this.props.zoom}
           google={this.props.google}
-          markers={this.state.locations}
           onReady={this.mapReady}
           onClick={this.closeInfoWindow}
-          style={style}
-          center={this.props.center}>
-{this.state.locations.map((location,index) =>
-  <Marker
+          style={mapStyle}>
+
+{this.state.filteredLocations.map((location,index) =>
+{let position={lat: location.lat,
+          lng: location.lng}
+
+return  <Marker
+    animation={(amProps? (((location.lat && location.lng) === (amProps.position.lat && amProps.position.lng))? 1 : 0):2)}
     key={index}
     name={location.name}
-    position={{lat: location.lat,
-              lng: location.lng}}
+    position={position}
     onClick={this.onMarkerClick}
-     />
+     />;
 
 
-)}
+ } )}
+
 
         <InfoWindow
           marker={this.state.activeMarker}
@@ -138,6 +143,7 @@ updateMarkers = (locations) => {
           </InfoWindow>
           </Map>
     </div>
+
     )
 }
 }
---
+++ src/Drawer.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+
+
+export default class Drawer extends Component {
+  constructor(props) {
+     super(props);
+     this.state={
+       query: '',
+       filteredLocations: []
+     }
+   }
+
+   componentDidUpdate(prevProps) {
+    if (this.props.filteredLocations !== prevProps.filteredLocations) {
+         this.setState({filteredLocations:this.props.filteredLocations});
+    } else {
+      return;
+    }
+
+   }
+
+updateQuery = (input) => {
+
+if (input==='' || !input) {
+  this.props.clearFilter();
+}
+ else {
+  this.props.onQueryUpdate(input);
+  return;
+}
+}
+
+
+
+  render() {
+
+
+     let {query} = this.state.query;
+
+
+    return (
+
+<div id='menuBody'>
+      <div className='searchbar'>
+        <input
+          type='search'
+          id='search'
+          name='filterLocations'
+          placeholder='Filter map markers'
+          value={query}
+          onChange={(event) => this.updateQuery(event.target.value)}/>
+      </div>
+
+<div id="showing-results-text">Showing {this.state.filteredLocations.length} Results:</div>
+<hr></hr>
+
+
+  {this.state.filteredLocations.map((item,index)=>
+ <li key={index} className="menu-item" >{item.name}</li>
+  )}
+</div>
+
+
+    )
+  }
+}
